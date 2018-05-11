49ddf0df78127428271e9677044cfdbdef691723
wintersalmon <salmonjoon@gmail.com>
2018-05-11 16:24:02 +0900

    improve run and update manager

--- tools/run.py
+++ tools/run.py
@@ -7,112 +7,72 @@ from .compare import compare_file_content
 from .settings import RES_DIR, SRC_DIR, TEMP_DIR
 
 
-def find_case_path(judge_alias, problem_id):
-    target_path = os.path.join(RES_DIR, judge_alias, problem_id)
-    cases = list()
-    for root, dirs, files in os.walk(target_path):
-        for file in files:
-            if file.endswith("in.txt"):
-                case_path = root
-                cases.append((os.path.split(root)[-1], case_path))
-    return sorted(cases)
+__all__ = [
+    'run_all_cases',
+    'run_one_case'
+]
 
 
-def run_all_script(judge_alias, problem_id, *, use_temp_files=True):
-    with open(
-            os.path.join(RES_DIR, judge_alias, problem_id, '.{}_{}.json'.format(judge_alias, problem_id))) as conf_file:
+def run_all_cases(judge_alias, problem_id, *, save_results=False, show_details=False):
+    conf_file_path = os.path.join(RES_DIR, judge_alias, problem_id, '.{}_{}.json'.format(judge_alias, problem_id))
+    with open(conf_file_path, 'r') as conf_file:
         conf = json.load(conf_file)
 
-    base_dir = os.path.join(SRC_DIR, judge_alias, problem_id)
-    # cases = find_case_path(judge_alias, problem_id)
-    source_file_path = os.path.join(base_dir, '{j_id}_{p_id}'.format(j_id=judge_alias, p_id=problem_id) + '.py')
-
     print('RUNNING {} {}'.format(judge_alias, problem_id))
+
     for case_id, case in conf['cases'].items():
-        print('- with case {}:'.format(case_id), end=' ')
-
         if 'skip' in case and case['skip'] is True:
-            run_time = None
-            run_result = 'SKIP'
+            print('- with case {}: [       ] SKIP'.format(case_id))
         else:
-            input_file_path = os.path.join(RES_DIR, judge_alias, problem_id, '.'.join(('in', case_id, 'txt')))
-            output_file_path = os.path.join(RES_DIR, judge_alias, problem_id, '.'.join(('out', case_id, 'txt')))
-
-            if use_temp_files:
-                case_temp_file = tempfile.NamedTemporaryFile()
-                result_file_path = case_temp_file.name
-            else:
-                case_temp_file = None
-                result_file_path = os.path.join(TEMP_DIR, '.'.join((judge_alias, problem_id, case_id, 'txt')))
-
-            run_time = run_script(
-                in_file_path=input_file_path, out_file_path=result_file_path, src_file_path=source_file_path)
-
-            cmp_result = compare_file_content(from_file_path=output_file_path, to_file_path=result_file_path)
-
-            if cmp_result:
-                run_result = 'FAIL'
-            else:
-                run_result = 'OK'
-
-            if use_temp_files:
-                case_temp_file.close()
-
-        # print('[{run_time:7.4f}] {run_result}'.format(run_time=run_time, run_result=run_result))
-        if run_time is None:
-            print('[       ] {run_result}'.format(run_result=run_result))
-        else:
-            print('[{run_time:7.4f}] {run_result}'.format(run_time=run_time, run_result=run_result))
+            show_run_and_compare(judge_alias, problem_id, case_id, save_results=save_results, show_details=show_details)
 
     print('DONE')
 
 
-def run_one_script(judge_alias, problem_id, case_id, *, use_temp_files=True):
-    with open(
-            os.path.join(RES_DIR, judge_alias, problem_id, '.{}_{}.json'.format(judge_alias, problem_id))) as conf_file:
-        conf = json.load(conf_file)
-
-    base_dir = os.path.join(SRC_DIR, judge_alias, problem_id)
-    # cases = find_case_path(judge_alias, problem_id)
-    source_file_path = os.path.join(base_dir, '{j_id}_{p_id}'.format(j_id=judge_alias, p_id=problem_id) + '.py')
-
+def run_one_case(judge_alias, problem_id, case_id, *, save_results=False, show_details=False):
     print('RUNNING {} {}'.format(judge_alias, problem_id))
-    print('- with case {}:'.format(case_id), end=' ')
-    input_file_path = os.path.join(RES_DIR, judge_alias, problem_id, '.'.join(('in', case_id, 'txt')))
-    output_file_path = os.path.join(RES_DIR, judge_alias, problem_id, '.'.join(('out', case_id, 'txt')))
+    show_run_and_compare(judge_alias, problem_id, case_id, save_results=save_results, show_details=show_details)
+    print('DONE')
 
-    if use_temp_files:
-        case_temp_file = tempfile.NamedTemporaryFile()
-        result_file_path = case_temp_file.name
+
+def show_run_and_compare(judge_id, problem_id, case_id, *, save_results=False, show_details=False):
+    print('- with case {}: '.format(case_id), end='')
+
+    running_time, compare_result = run_and_compare(judge_id, problem_id, case_id, save_results=save_results)
+
+    running_time_fmt = '{:7.4f}'.format(running_time)
+    compare_result_fmt = 'OK' if not compare_result else 'FAIL'
+
+    print('[{}] {}'.format(running_time_fmt, compare_result_fmt))
+
+    if show_details:
+        for line in compare_result:
+            print(line)
+
+
+def run_and_compare(judge_id, problem_id, case_id, *, save_results=False):
+    base_dir = os.path.join(SRC_DIR, judge_id, problem_id)
+    source_file_path = os.path.join(base_dir, '{j_id}_{p_id}'.format(j_id=judge_id, p_id=problem_id) + '.py')
+    input_file_path = os.path.join(RES_DIR, judge_id, problem_id, '.'.join(('in', case_id, 'txt')))
+    output_file_path = os.path.join(RES_DIR, judge_id, problem_id, '.'.join(('out', case_id, 'txt')))
+
+    if save_results:
+        temp_result_file = None
+        result_file_path = os.path.join(TEMP_DIR, '.'.join((judge_id, problem_id, case_id, 'txt')))
     else:
-        case_temp_file = None
-        result_file_path = os.path.join(TEMP_DIR, '.'.join((judge_alias, problem_id, case_id, 'txt')))
+        temp_result_file = tempfile.NamedTemporaryFile()
+        result_file_path = temp_result_file.name
 
-    run_time = run_script(
-        in_file_path=input_file_path, out_file_path=result_file_path, src_file_path=source_file_path)
-
+    exe_time = run(in_file_path=input_file_path, out_file_path=result_file_path, src_file_path=source_file_path)
     cmp_result = compare_file_content(from_file_path=output_file_path, to_file_path=result_file_path)
 
-    if cmp_result:
-        run_result = 'FAIL'
-    else:
-        run_result = 'OK'
+    if temp_result_file:
+        temp_result_file.close()
 
-    for line in cmp_result:
-        print(line)
-
-    if use_temp_files:
-        case_temp_file.close()
-
-    if run_time is None:
-        print('[       ] {run_result}'.format(run_result=run_result))
-    else:
-        print('[{run_time:7.4f}] {run_result}'.format(run_time=run_time, run_result=run_result))
-
-    print('DONE')
+    return exe_time, cmp_result
 
 
-def run_script(in_file_path, out_file_path, src_file_path):
+def run(in_file_path, out_file_path, src_file_path):
     start = time.time()
     os.system(
         "cat {in_file_path} | python {src_file_path} > {out_file_path}".format(
